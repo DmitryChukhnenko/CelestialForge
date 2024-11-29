9050cfa24e322599085bbfe6fe605c48978903c1
ChukhnenkoDA <CiberKnight2018@gmail.com>
2024-11-29 16:51:16 +0300

    Project Initialization

---
+++ src/main/java/Dmitry/CelestialForge/CelestialForgeApplication.java
@@ -0,0 +1,13 @@
+package Dmitry.CelestialForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelestialForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelestialForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Dmitry/CelestialForge/CelestialForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package Dmitry.CelestialForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelestialForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
